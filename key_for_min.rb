98994778b617a080527f244a5a4d6e8d1bd60938
Eugene O'Neil <ejo7220@gmail.com>
2020-07-31 19:14:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
 min_value = 0 
 lowest_key = nil 
-name_hash.do |key,value|
+name_hash.each do |key,value|
   if min_value == 0 || value < min_value
     min_value = value
     lowest_key = key
